1c681a4c5753f45e81c83ccc0b4c9acf54af0346
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-07-07 23:20:38 +0300

    edit footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,13 +17,13 @@ const Footer = () => {
                 <MagicButton 
                     title="Let's get in touch"
                     icon={<FaLocationArrow />}
-                    position='right'
+                    position="right"
                 />
             </a>
         </div>
-        <div className='flex mt-16 md:flex-cow flex-col justify-between items-center'>
-            <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 Esma Nazlı</p>
-            <div className='flex items-center md:gap-3 gap-6'>
+        <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+            <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Esma Nazlı</p>
+            <div className="flex items-center md:gap-3 gap-6">
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                         <img 
